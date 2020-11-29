e7043f934f65f659e5b01e0eed00bd983630d96f
vapormusic <customer.lofi@gmail.com>
2020-11-29 12:35:38 +0700

    progress

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -14,10 +14,16 @@ import AVFoundation
 struct ContentView: View {
       
     @State var audioPlayer:AVPlayer?
+    @State var albumarturl: String?
+    
 
      @State var isPlaying : Bool = false
     var body: some View {
-        AnimatedImage(url: URL(string: "https://i.pinimg.com/originals/16/69/e5/1669e57761ccc67fa5e31a09a54764d0.gif"))
+        ZStack {
+            
+          
+            
+            AnimatedImage(url: URL(string: "https://i.pinimg.com/originals/16/69/e5/1669e57761ccc67fa5e31a09a54764d0.gif"))
             
             
             // Supports options and context, like `.delayPlaceholder` to show placeholder only when error
@@ -32,29 +38,43 @@ struct ContentView: View {
             }
             .indicator(.activity) // Activity Indicator
             .transition(.fade(duration: 0.5))// Fade Transition with duration
-        
-        
-        
-    
-        Button(action: {
-           //  Sounds.playSounds(soundfile: "a.wav")
-            Sounds.playHttpSounds(soundfile: "https://data35.chiasenhac.com/downloads/1954/6/1953381-6ce0e25f/128/Slow%20Dancing%20In%20The%20Dark%20-%20Joji.mp3")
+            
+            ZStack(alignment: Alignment(horizontal: .center, vertical: .top)){
+            Image("frameworkmac")
+               
+                VStack{
+                    Spacer()
+                        .frame(height: 40.0)
+                WebImage(url: URL(string:"https://images.genius.com/79c6343980b4513f2c46813301da0dec.300x300x1.png"))
+                    .resizable()
+                  // .scaledToFit()
+                
+                    .placeholder {
+                    Rectangle().foregroundColor(.gray)
+                }
+                   .padding(.top,40)
+                   .padding(.leading,4)
+                    .padding(.trailing,23)
+                    .scaledToFill()
+                    .frame(width: 269, height: 157)
+                    .clipped()
+                    
+                    
+           
+                
+                }
+            }.frame(width: 269, height: 380)
+            Spacer()
+            Button(action: {
+               //  Sounds.playSounds(soundfile: "a.wav")
+                Sounds.playHttpSounds(soundfile: "https://data35.chiasenhac.com/downloads/1954/0/1953381-6ce0e25f/128/Slow%20Dancing%20In%20The%20Dark%20-%20Joji.mp3")
 
-        }) {
-            HStack {
-                Image(systemName: "trash")
-                    .font(.title)
-                Text("Delete")
-                    .fontWeight(.semibold)
-                    .font(.title)
-            }
-            .frame(minWidth: 0, maxWidth: .infinity)
-            .padding()
-            .foregroundColor(.white)
-            .background(LinearGradient(gradient: Gradient(colors: [Color("DarkGreen"), Color("LightGreen")]), startPoint: .leading, endPoint: .trailing))
-            .cornerRadius(40)
+            }) {}
+            .padding(.bottom, 30.0)
+            .frame(width: 10.0, height: 52.0)
+    
+
         }
-        
  
       
         
